6f6e32d1b5568a6808393c65a1d982dfb49b8cdd
Aishwarya TS <aishwaryatsgowda02@gmail.com>
2026-09-12 01:15:59 +0530

    feat: add enter key to create todo

--- src/components/InputContainer.jsx
+++ src/components/InputContainer.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
 function InputContainer({ inputVal, writeTodo, addTodo }) {
+
+  function handleKeyDown(e) {
+    if (e.key === 'Enter') {
+      addTodo();
+    }
+  }
+
   return (
     <div className="input-container">
       <input
         type="text"
         value={inputVal}
         onChange={writeTodo}
+        onKeyDown={handleKeyDown}
       />
 
       <button onClick={addTodo}>+</button>
